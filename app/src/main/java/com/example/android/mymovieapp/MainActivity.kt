13cf6e621e6424f918faa07bc59a140c48ffa8c4
Taiwo Lawal <taiwolawal360@gmail.com>
2021-03-11 08:02:53 +0100

    Added dependencies for Viewbinding, Room, Coroutine, Glide, Recyclerview, Cardview, Viewmodel and Livedata.

--- app/src/main/java/com/example/android/mymovieapp/MainActivity.kt
+++ app/src/main/java/com/example/android/mymovieapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.android.mymovieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var searchMenuItem: MenuItem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.movie_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        return super.onCreateOptionsMenu(menu)
+
+
     }
 }
